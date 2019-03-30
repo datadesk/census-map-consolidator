cf0961737e9f8e5a0af2656513521996103f2832
palewire <ben.welsh@gmail.com>
2019-03-30 07:51:44 -0700

    v0.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='census-consolidator',
-    version='0.0.3',
+    version='0.0.4',
     description="Combine Census blocks into new shapes",
     author='Ben Welsh',
     author_email='ben.welsh@gmail.com',
